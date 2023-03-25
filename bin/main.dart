42968ea6be96909acf1a7eaadb018d9df95d4d4f
Richard Macwoods <59056174+Ultra-rrich@users.noreply.github.com>
2023-03-25 14:19:07 -0700

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -7,3 +7,25 @@ main(List<String> arguments) {
     if(i == 5) print('Half way there');
   }
 }
+
+
+My Answer
+int number;
+int small = 1;
+it large = 11;
+
+value = large;
+
+do {
+	(print('number');
+	number++;
+
+
+if ( number == 5) {
+
+	print('half way there');
+	continue;
+
+} while (number < large);
+
+}
